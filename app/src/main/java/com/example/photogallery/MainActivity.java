15bf2c82c2091d7122f668b2fa4dead17ccd7af1
jle-santos <49331367+jle-santos@users.noreply.github.com>
2020-02-03 19:30:35 -0800

    Added share to Messenger

--- app/src/main/java/com/example/photogallery/MainActivity.java
+++ app/src/main/java/com/example/photogallery/MainActivity.java
@@ -338,7 +338,7 @@ public class MainActivity extends AppCompatActivity {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileName, "_",storageDir);
+        File image = File.createTempFile(imageFileName, "_.jpg",storageDir);
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
     }
--- app/src/main/java/com/example/photogallery/shareActivity.java
+++ app/src/main/java/com/example/photogallery/shareActivity.java
@@ -1,27 +1,81 @@
 package com.example.photogallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+
 
 public class shareActivity extends AppCompatActivity {
+
+    String filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
 
+        final Button btnTwitter = findViewById(R.id.btnTwitter);
+
         Intent intent = getIntent();
-        String filePath = intent.getStringExtra(MainActivity.Picture_Location);
+        filePath = intent.getStringExtra(MainActivity.Picture_Location);
 
         ImageView sharePreview = findViewById(R.id.ivShare);
         sharePreview.setImageBitmap(BitmapFactory.decodeFile(filePath));
 
         Log.i("Dev:: Sharing:",filePath);
+
+        btnTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Dev::", "Sharing to Messenger");
+
+                File file = new File(filePath);
+
+                String packageName = "com.facebook.orca";
+
+                Uri imageUri = FileProvider.getUriForFile(getApplicationContext(),
+                                            "com.example.photogallery.fileprovider",
+                                                    file);
+
+                Intent sendIntent = new Intent();
+                sendIntent.setType("image/*");
+                sendIntent.setAction(Intent.ACTION_SEND);
+                //sendIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                sendIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
+
+                //sendIntent.putExtra(Intent.EXTRA_SUBJECT, "SENT FROM MY ANDROID");
+                //sendIntent.putExtra(Intent.EXTRA_TEXT, "TEST FROM ANDROID");
+                //sendIntent.setType("text/plain");
+
+                sendIntent.setPackage(packageName);
+
+                try {
+                    startActivity(sendIntent);
+                }
+                catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(getApplicationContext(), "No Application Found", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+            }
+
+
+
+        });
+
 
     }
 }
